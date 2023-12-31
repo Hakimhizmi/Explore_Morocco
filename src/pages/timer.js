f9990cfa24d42495e9782a74ab18c5070001f4dd
Hakimhizmi <hakimhizmi93@gmail.com>
2023-12-31 16:00:49 +0100

    Sun Dec 31, 2023, 4:00 PM +01:00

---
+++ src/pages/timer.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+
+export default function Timer() {
+    const [count, setCount] = useState(6);
+
+    useEffect (() => {
+        let myInterval = setInterval(() => {
+            setCount(count - 1)
+        }, 1000);
+        // Clear the interval when the component unmounts or when count becomes false
+        return () => {
+            clearInterval(myInterval)
+        }
+    })
+  return (
+    <h1 className='text-3xl text-white  italic'>{count}</h1>
+  )
+}
